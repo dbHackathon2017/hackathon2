4bc7a6a73b3cf361abffb622829eb049a99494b6
Robin de Boer <robindboer@gmail.com>
2017-02-11 17:10:44 +0100

    Add styiling

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ mainApp.controller("loginController",["$scope", "$routeParams", "$timeout", "$lo
 				loginScope.loading = false;
 				$cookies.put(COOKIE_LOGGED_IN, true);
 				$location.path("pensions");
-			}, 1200);
+			}, 1400);
 		}
 	}
 ]);
